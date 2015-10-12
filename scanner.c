6189e00837f56d3c2f35452654d28e10444d1ead
Irimitenkan <RohacekD@gmail.com>
2015-10-12 16:15:53 +0200

    Upravy FSM

--- scanner.c
+++ scanner.c
@@ -11,18 +11,36 @@ enum states {
 	ERROR
 };
 
+/*
+* KEY WORDS
+* auto, cin, cout, double, else,
+* for, if, int, return, string
+*
+*
+*/
+/*
+ * Musi vracet nìjakou reprezentaci tokenu
+ * Dostávat ukazatel na otevøený soubor (DI)
+ */
 smth getTocen(char*, smth, smth) {
+
+	/* @var prevRest int poslední naètený znak (Zbytek posledního prùchodu) */
+	static int prevRest = -1;
+	/* @var c int actual character*/
+	static int c = -1;
+
 	int state = START;
-	/* @var c char actual character*/
-	char c;
 	while (1) {
-		c = getc(char*);
+		c = getc(FILE*);
 		
 		switch (state)
 		{
 		case START:
 			if (c == 35) {
 				state = STRING;
+			}
+			else if (isspace(c)) {
+				state = START;
 			}
 			break;
 		default:
