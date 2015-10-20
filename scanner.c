7b182ac663a69764d9a2364a29ab02935d1734da
Irimitenkan <RohacekD@gmail.com>
2015-10-21 01:09:55 +0200

    FSM komentáře

--- scanner.c
+++ scanner.c
@@ -9,7 +9,12 @@ enum states {
 	STRING,
 	STRING_END,
 	STRING_ESCAPE,
-	ERROR
+	ERROR,
+	DIVISION_COMMENT,
+	BLOCK_COMMENT,
+	LINE_COMMENT,
+	NESTED_BLOCK_COMMENT_CHECK,
+	END_BLOCK_COMMENT
 };
 /* @var int citac radku*/
 int line = 1;
@@ -48,6 +53,9 @@ int getToken(int* p, FILE* source) {
 			else if (c == "\"") {
 				state = STRING;
 			}
+			else if (c == "/") {
+
+			}
 			break;
 		case STRING:
 			if (c == EOF) {
@@ -64,6 +72,64 @@ int getToken(int* p, FILE* source) {
 			}
 			else {//uloz si znak
 
+			}
+			break;
+		case DIVISION_COMMENT:
+			if (c == "*") {
+				state = BLOCK_COMMENT;
+			}
+			else if (c == "/") {
+				state = LINE_COMMENT;
+			}
+			else {//jednoznakovy operator deleni
+
+			}
+			break;
+		case BLOCK_COMMENT:
+			if (c == EOF) {
+				//TODO: error
+			}
+			else if (c=="/") {
+				state = NESTED_BLOCK_COMMENT_CHECK;
+			}
+			else if (c=="*") {
+				state = END_BLOCK_COMMENT;
+			}
+			break;
+		case LINE_COMMENT:
+			if (c == "\n") {//konec radkoveho komentare hledame dalsi lexem
+				state = START;
+			}
+			else if (c == EOF) {//TODO: je to chyba??
+
+			}
+			else {//komentar pokracuje
+				
+			}
+			break;
+		case END_BLOCK_COMMENT:
+			if (c == "*") {
+				//zùstaneme tady komentáø mùže konèit x hvìzdièkama
+			}
+			else if (c == EOF) {
+				//todo: error
+			}
+			else if (c == "/") {//konec blokového komentáøe jdeme hledat další lexém
+				state = START;
+			}
+			else {
+				state = BLOCK_COMMENT;
+			}
+			break;
+		case NESTED_BLOCK_COMMENT_CHECK:
+			if (c == "*") {
+				//TODO: error 
+			}
+			else if (c == EOF) {
+				//TODO: error
+			}
+			else {
+				state = BLOCK_COMMENT;
 			}
 			break;
 		default:
