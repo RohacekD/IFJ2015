810471f3821eed7c16018fdd80de995a01f2dfe5
xdocek09 <xdocek09@stud.fit.vutbr.cz>
2015-11-21 22:25:37 +0100

    Predelan klic na string.

---
+++ ial.c
@@ -0,0 +1,284 @@
+/*
+ * ial.c
+ *  Project: interpreter
+ *  Created on: 18. 10. 2015
+ *  Author: xdocek09
+ *  Description: Vestavene funkce pro IAL.
+ */
+/**
+ * @file ial.c
+ * @author xdocek09
+ * @brief Vestavene funkce pro IAL.
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include "ial.h"
+
+int len(char* string) {
+	int num = 0;
+	while (string[num] != 0)
+		num++;
+	return num;
+}
+
+void computeJumps(char* string, int charJump[]) {
+	int lengthString = len(string);
+
+	for (int c = 0; c < 256; c++) {
+
+		//plnime maximalnim skokem
+		charJump[c] = lengthString;
+
+	}
+
+	for (int k = 0; k < lengthString; k++) {
+		charJump[string[k]] = lengthString - k - 1;
+	}
+}
+
+int BMA(char* string, char* searchString, int charJump[]) {
+
+	int stringLength = len(string);
+	int searchStringLength = len(searchString);
+	int j = searchStringLength - 1;	//index v textu
+	int k = searchStringLength - 1;	//index ve vzorku
+
+	while (j < stringLength && k >= 0) {
+
+		if (string[j] == searchString[k]) {
+			j--;
+			k--;
+		} else {
+			j = j + charJump[string[j]];
+			k = searchStringLength - 1;
+		}
+	}
+	if (k == -1) {
+		return j + 1;
+	} else {
+		return stringLength;
+	}
+}
+/**
+ * Pouziva funkce heapsort.
+ * @param string
+ * @param l
+ * @param r
+ */
+void siftDown(char * string, int l, int r) {
+	int i = l;
+	int j = 2 * i;
+	int con = (j <= r);
+	int tmp = string[i];
+
+	while (con) {
+		if (j < r) {
+			if (string[j] < string[j + 1]) {
+				j++;
+			}
+		}
+		if (tmp >= string[j]) {
+			con = 0;
+		} else {
+			string[i] = string[j];
+			i = j;
+			j = 2 * i;
+			con = (j <= r);
+		}
+
+	}
+	string[i] = tmp;
+}
+
+void heapSort(char* string) {
+	int stringLength = len(string);
+	int l;
+	int r;
+	char tmp;
+	l = stringLength / 2;
+	r = stringLength - 1;
+
+	for (int i = l; i >= 0; i--) {
+		siftDown(string, i, r);
+	}
+
+	for (r = stringLength - 1; r > 0; r--) {
+
+		tmp = string[0];
+		string[0] = string[r];
+		string[r] = tmp;
+
+		siftDown(string, 0, r - 1);
+	}
+}
+
+int BSTSearch(tBSTNodePtr rootPtr, string* key) {
+	if (rootPtr == NULL) {
+		return 0;
+	}
+
+	if (strCmpString(rootPtr->key,key) == 0 ) {
+		//nalezeno
+		return 1;
+	}
+	if (strCmpString(rootPtr->key,key) < 0) {
+		//hledani pokracuje v pravem podstromu
+		return BSTSearch(rootPtr->r, key);
+	} else {
+		//hledani pokracuje v levem podstromu
+		return BSTSearch(rootPtr->l, key);
+	}
+}
+
+void BSTSearchTree(tBSTNodePtr rootPtr, string* key, tBSTNodePtr* store) {
+	if(store==NULL){
+		//chyba
+		return;
+	}
+	if (rootPtr == NULL) {
+		*store = NULL;
+		return;
+	}
+
+	if (strCmpString(rootPtr->key,key) == 0) {
+		//nalezeno
+		*store = rootPtr;
+		return;
+	}
+	if (strCmpString(rootPtr->key,key) < 0) {
+		//hledani pokracuje v pravem podstromu
+		BSTSearchTree(rootPtr->r, key, store);
+	} else {
+		//hledani pokracuje v levem podstromu
+		BSTSearchTree(rootPtr->l, key, store);
+	}
+
+}
+
+/**
+ * Vytvari uzel.
+ * @param node	-	ukazatel pro alokaci uzlu
+ * @param key	-	klic noveho uzlo
+ * @param data	-	data noveho uzlo
+ */
+void BSTCreateNode(tBSTNodePtr* node, string* key, tData* data) {
+	tBSTNodePtr newNode = malloc(sizeof(struct tBSTNode));
+	string* newString = malloc(sizeof(string));
+	if (newNode == NULL || newString==NULL) {
+		//pokus o alokaci se nazdaril
+		return;
+	}
+	//naplneni stringu
+	strInit(newString);
+	strCopyString(newString, key);
+
+	newNode->key = newString;
+	newNode->data = data;
+	newNode->l = NULL;
+	newNode->r = NULL;
+	*node=newNode;
+}
+void BSTInsert(tBSTNodePtr* rootPtr, string* key, tData* data) {
+	if(rootPtr == NULL){
+		//chyba nelze
+		return;
+	}
+	if ((*rootPtr) == NULL) {
+		//vytvorime novy uzel
+		BSTCreateNode(rootPtr, key, data);
+		return;
+	}
+
+	tBSTNodePtr workWithRootPtr=*rootPtr;//pro zprehledneni kodu
+
+	if (strCmpString(key,workWithRootPtr->key)<0) {
+		//pokracujeme levym podstrom
+		BSTInsert(&(workWithRootPtr->l), key, data);
+		return;
+	}
+	if (strCmpString(key,workWithRootPtr->key)>0) {
+		//pokracujeme pravym podstrom
+		BSTInsert(&(workWithRootPtr->r), key, data);
+		return;
+	}
+	//aktualizace
+	workWithRootPtr->data = data;
+}
+
+/**
+ * Vyhledava uzel pro odstraneni (jedna se o nejpravejsi uzel). Pokud najde prepise
+ * s nim assistPtr.
+ * @param rootPtr	-	ukazatel na korenovy uzel leveho podstromu ruseneho uzlu.
+ * @param assistPtr	-	Po provedeni je zde ulozen odkaz na uzel pro odstraneni.
+ */
+void BSTDeleteVictimSearch(tBSTNodePtr* rootPtr, tBSTNodePtr* assistPtr) {
+	if (rootPtr==NULL){
+		//chyba
+		return;
+	}
+	tBSTNodePtr workWithRootPtr=*rootPtr;//pro zprehledneni kodu
+	if (workWithRootPtr->r != NULL) {
+		//pokracujeme v pravem podstromu
+		BSTDeleteVictimSearch(&(workWithRootPtr->r), assistPtr);
+		return;
+	}
+	//nejpravejsi ukazatel je nalezen
+	//prepiseme uzel
+	(*assistPtr)->data = workWithRootPtr->data;
+	(*assistPtr)->key = workWithRootPtr->key;
+	(*assistPtr) = workWithRootPtr;
+
+	//uvolnime uzel
+	*rootPtr=workWithRootPtr->l;	/**todo Proc to tu je?*/
+
+}
+
+/**
+ * Uvolni z pameti uzel
+ * @param node[in]	-	Uzel pro uvolneni z pameti.
+ */
+void BSTFreeNode(tBSTNodePtr node) {
+	strFree(node->key);//musime uvolnit retezec z pameti
+	free(node->key);
+	free(node);
+}
+
+void BSTDelete(tBSTNodePtr* rootPtr, string* key) {
+	if (rootPtr==NULL && *rootPtr == NULL) {
+		//chyba / konec vyhledavani
+		return;
+	}
+
+	tBSTNodePtr workWithRootPtr=*rootPtr;//pro zprehledneni kodu
+	if (strCmpString(key,workWithRootPtr->key)<0) {
+		//pokracujeme levym podstromem
+		BSTDelete(&(workWithRootPtr->l), key);
+		return;
+	}
+
+	if (strCmpString(key,workWithRootPtr->key)>0) {
+		//pokracujeme pravym podstromem
+		BSTDelete(&(workWithRootPtr->r), key);
+		return;
+	}
+
+	//jsme na klici=>rusime ukazatel rootPtr
+	tBSTNodePtr assistPtr = workWithRootPtr;
+	if (assistPtr->r == NULL) {
+		//uzel nema pravy podstrom, pripojime levy podstrom
+		*rootPtr = assistPtr->l;
+	} else {
+		if (assistPtr->l == NULL) {
+			//uzel nema levy podstrom, pripojime pravy podstrom
+			*rootPtr = assistPtr->r;
+			return;
+		} else {
+			//prohledame levy podstrom pro nejpravejsi uzel
+			BSTDeleteVictimSearch(&(assistPtr->l), &assistPtr);
+		}
+
+		BSTFreeNode(assistPtr);
+	}
+}
+
+/*** End of file: ial.c ***/
---
+++ ial.h
@@ -0,0 +1,142 @@
+/*
+ * ial.h
+ *  Project: interpreter
+ *  Created on: 18. 10. 2015
+ *  Author: xdocek09
+ *  Description: Hlavickovy soubor (pro ial.c) vestavenych funkci pro IAL.
+ */
+/**
+ * @file ial.h
+ * @author xdocek09
+ * @brief Hlavickovy soubor (pro ial.c) vestavenych funkci pro IAL.
+ */
+
+#ifndef IAL_H_
+#define IAL_H_
+
+#include "str.h"
+#include <stdbool.h>
+typedef enum
+{
+	TAB_SYM_VARIABLE,
+	TAB_SYM_FUNCTION
+}tTabSymDataType;
+
+typedef enum
+{
+	TAB_SYM_VAR_INTEGER,
+	TAB_SYM_VAR_DOUBLE,
+	TAB_SYM_VAR_STRING,
+	TAB_SYM_VAR_BOOLEAN,
+	TAB_SYM_VAR_AUTO
+}tTabSymVarDataType;
+
+typedef enum
+{
+	TAB_SYM_RET_INTEGER,
+	TAB_SYM_RET_DOUBLE,
+	TAB_SYM_RET_STRING,
+	TAB_SYM_RET_BOOLEAN
+}tTabSymRetDataType;
+
+typedef struct{
+	tTabSymVarDataType dataType;
+}tVariableInfo;
+
+
+typedef struct tParamListElem {
+    struct tParamListElem *next;
+    string idName;
+    tTabSymVarDataType dataType;
+} *tParamListElemPtr;
+
+typedef struct{
+	tParamListElemPtr first;
+}*tParamListPtr;
+
+
+typedef struct{
+	tParamListPtr params;		// parametry funkce
+	tTabSymRetDataType retType;	// navratovy typ funkce
+	tBSTNodePtr	locTab;			// lokalni tabulka symbolu
+	bool defined;				// byla definovana
+
+}tFuncInfo;
+
+
+typedef struct{
+	tTabSymDataType type;	//druh identifikatoru
+	union{
+		tVariableInfo *var;
+		tFuncInfo * func;
+	}info;
+}tData;
+
+typedef struct tBSTNode{
+	string* key;
+	tData * data;
+	struct tBSTNode *l;
+	struct tBSTNode *r;
+
+} *tBSTNodePtr;
+
+
+int len(char* string);
+/**
+ * Vypocita u retezce, hodnoty skoku pro dané znaky.
+ * @param string	-	retezec
+ * @param charJump	-	zde se ulozi hodnoty skoku (indexujeme pomoci znaku)
+ */
+void computeJumps(char* string, int charJump[]);
+
+
+/**
+ * Stanovi index pocatecni polohy vzorku v hledanem textu.
+ * @param string		-	text pro vyhledavani
+ * @param searchString	-	retezec, ktery se vyhledava
+ * @param charJump		-	predvypocitane hodnoty skoku pro searchString pomoci computeJumps
+ * @return Vraci index pocatecni polohy vzorku v danem textu. Pri neuspechu
+ * 	vraci maximalni index stringu +1.
+ */
+int BMA(char* string, char* searchString, int charJump[]);
+
+/**
+ * Razeni pomoci heap sort
+ * @param string	-	razeny retezec
+ */
+void heapSort(char* string);
+
+/**
+ * Vyhledava v binarnim vyhledavacim stromu
+ * @param rootPtr	-	ukazatel na koren stromu
+ * @param key		-	vyhledavany klic
+ * @return	0-nenalezeno, 1-nalezeno
+ */
+int BSTSearch(tBSTNodePtr rootPtr, string* key);
+
+/**
+ * Vyhledava v binarnim vyhledavacim stromu. Vraci ukazatel na nalezeny uzel jinak NULL.
+ * @param rootPtr	-	ukazatel na koren stromu
+ * @param key		-	vyhledavany klic
+ * @param store		-	 Vraci zde ukazatel na nalezeny uzel nebo NULL.
+ */
+void BSTSearchTree(tBSTNodePtr rootPtr, string* key, tBSTNodePtr* store);
+
+/**
+ * Vklada do binarniho vyhledavaciho stromu novy uzel nebo
+ * aktualizuje stary v pripade, ze klic jiz ve stromu existuje.
+ * @param rootPtr	-	ukazatel na koren stromu
+ * @param key		-	klic vkladaneho/aktualizovaneho uzlu
+ * @param data		-	data vkladaneho/aktualizovaneho uzlu
+ */
+void BSTInsert(tBSTNodePtr* rootPtr, string* key, tData* data);
+
+/**
+ * Maze z binarniho vyhledavaciho stromu uzel s danym klicem key.
+ * @param rootPtr	-	ukazatel na koren stromu
+ * @param key		-	klic uzlu, ktery ma byt odstranen
+ */
+void BSTDelete(tBSTNodePtr* rootPtr, string* key);
+
+#endif /* IAL_H_ */
+/*** End of file: ial.h ***/
