2d9ac077eb60be2fcf75fa7cb661dbe64bc950a4
xdocek09 <xdocek09@stud.fit.vutbr.cz>
2015-11-23 18:31:59 +0100

    Implementace instrukcni pasky.

--- insTape.c
+++ insTape.c
@@ -11,7 +11,295 @@
  * @brief  Implementace instrukcni pasky, pomoci obousmerne vazaneho seznamu.
  */
 
+#include <stdlib.h>
+#include "insTape.h"
 
+tInsTape* insTapeCreate() {
+	tInsTape* newTape = malloc(sizeof(tInsTape));
 
+	if (newTape != NULL) {
+		//inicializace
+		newTape->act = NULL;
+		newTape->first = NULL;
+		newTape->last = NULL;
+	}
 
+	return newTape;
+}
+
+/**
+ * Uvolni instrukcni pasku z pameti.
+ * @param tape[in]	-	instrukcni paska pro uvolneni.
+ */
+void insTapeFree(tInsTape* tape) {
+	//uvolnime prvky z pameti
+	tInsTapeInsPtr act;
+	tInsTapeInsPtr next = tape->first;
+
+	while (next != NULL) {
+		act = next;
+		next = act->rptr;
+		free(act);
+	}
+	//uvolnime z pameti pasku samotnou
+	free(tape);
+}
+
+/**
+ * Vytvari novou instrukci.
+ * @param tape[in]	-	Paska kam vlozi instrukci.
+ * @param type[in]	-	Druh instrukce.
+ * @param adr1[in]	-	Prvni adresa.
+ * @param adr2[in]	-	Druha adresa.
+ * @param adr3[in]	-	Treti adresa.
+ * @param lptr[in]	-	ukazatel na predchozi instrukci
+ * @param rptr[in]	-	ukazatel na nasledujici instrukci
+ * @return	Vraci ukazatel na novou instrukci. Pri chybe vraci NULL.
+ */
+tInsTapeInsPtr createInstruction(tInstructTypes type, void* adr1, void* adr2,
+		void* adr3, tInsTapeInsPtr lptr, tInsTapeInsPtr rptr) {
+
+	tInsTapeInsPtr newIns = malloc(sizeof(struct tInsTapeIns));
+	if (newIns != NULL) {
+		//nastavime hodnoty
+		newIns->type = type;
+		newIns->adr1 = adr1;
+		newIns->adr2 = adr2;
+		newIns->adr3 = adr3;
+		newIns->lptr;
+		newIns->rptr;
+	}
+	return NULL;
+}
+
+int insTapeInsertFirst(tInsTape* tape, tInstructTypes type, void* adr1,
+		void* adr2, void* adr3) {
+	//pokusime se alokovat pamet pro novou instrukci
+	tInsTapeInsPtr newIns = createInstruction(type, adr1, adr2, adr3, NULL,
+			tape->first);
+
+	if (newIns == NULL) {
+		//chyba
+		return 0;
+	}
+
+	//zacleneni nove instrukce
+
+	if (tape->first == NULL) {
+		//prvek je jedinny
+		tape->last = newIns;
+	} else {
+		tape->first->lptr = newIns;
+	}
+	tape->first = newIns;
+	return 1;
+}
+
+int insTapeInsertLast(tInsTape* tape, tInstructTypes type, void* adr1,
+		void* adr2, void* adr3) {
+
+	//pokusime se alokovat pamet pro novou instrukci
+	tInsTapeInsPtr newIns = createInstruction(type, adr1, adr2, adr3,
+			tape->last, NULL);
+
+	if (newIns == NULL) {
+		//chyba
+		return 0;
+	}
+
+	//zacleneni nove instrukce
+
+	if (tape->last == NULL) {
+		//prvek je jedinny
+		tape->first = newIns;
+	} else {
+		tape->last->rptr = newIns;
+	}
+	tape->last = newIns;
+	return 1;
+}
+
+void insTapeFirst(tInsTape* tape) {
+	tape->act = tape->first;
+}
+
+void insTapeLast(tInsTape* tape) {
+	tape->act = tape->last;
+}
+
+tInsTapeInsPtr insTapeGetFirst(tInsTape* tape) {
+	return tape->first;
+}
+
+tInsTapeInsPtr insTapeGetLast(tInsTape* tape) {
+	return tape->last;
+}
+
+void insTapeDeleteFirst(tInsTape* tape) {
+	if (tape->first == NULL) {
+		return;
+	}
+
+	//zkontrolujeme, jestli nahodou neni prvni aktivni
+	if (tape->first == tape->act) {
+		// rusime aktivitu
+		tape->act = NULL;
+	}
+
+	//smazeme prvni prvek a nastavim druhy prvek jako prvni
+	tInsTapeInsPtr newFirst = tape->first->rptr;
+	if (newFirst == NULL) {
+		//prvni byl i posledni
+		tape->last = NULL;
+	} else {
+		//nove prvni
+		newFirst->lptr = NULL;
+	}
+	free(tape->first);
+	tape->first = newFirst;
+}
+
+void insTapeDeleteLast(tInsTape* tape) {
+	if (tape->last == NULL) {
+		return;
+	}
+
+	//zkontrolujeme, jestli nahodou neni posledni aktivni
+	if (tape->last == tape->act) {
+		// rusime aktivitu
+		tape->act = NULL;
+	}
+
+	//smazeme posledni prvek a nastavime predposledni jako posledni
+	tInsTapeInsPtr newLast = tape->last->lptr;
+	if (newLast == NULL) {
+		//posledni byl i prvni
+		tape->first = NULL;
+	} else {
+		//nove posledni
+		newLast->rptr = NULL;
+	}
+	free(tape->last);
+	tape->last = newLast;
+}
+
+void insTapePostDelete(tInsTape* tape) {
+	if (tape->act == NULL || tape->last == tape->act) {
+		//paska neni aktivni, nebo je aktivni posledni instrukce
+		return;
+	}
+	//ulozime si odkaz na prvek za prvekem, ktery mazeme
+	tInsTapeInsPtr newPost = tape->act->rptr->rptr;
+	free(tape->act->rptr);
+	tape->act->rptr = newPost;
+
+	if (newPost != NULL) {
+		//v pripade, ze smazana instrukce nebyla posledni
+		newPost->lptr = tape->act;
+	} else {
+		//v pripade, ze smazana instrukce byla posledni
+		tape->last = tape->act;
+	}
+}
+
+void insTapePreDelete(tInsTape* tape){
+	if(tape->act==NULL || tape->first==tape->act){
+		//paska neni aktivni, nebo je aktivni prvni instrukce
+		return;
+	}
+	//ulozime si odkaz na instrukci pred prvekem, ktery mazeme
+	tInsTapeInsPtr newPre=tape->act->lptr->lptr;
+	free(tape->act->lptr);
+
+	tape->act->lptr=newPre;
+
+	if(newPre!=NULL){
+		//v pripade, ze smazana instrukce nebyla prvni
+		newPre->rptr=tape->act;
+	}else{
+		//v pripade, ze smazana instrukce byla prvni
+		tape->first=tape->act;
+	}
+}
+
+int insTapePostInsert(tInsTape* tape, tInstructTypes type, void* adr1,
+		void* adr2, void* adr3){
+	if(tape->act==NULL){
+		//paska neni aktivni
+		return 0;
+	}
+	//pokusime se alokovat pamet pro novou instrukci
+	tInsTapeInsPtr newInstruct=createInstruction(type, adr1, adr2, adr3, tape->act, tape->act->rptr);
+
+	if(newInstruct==NULL){
+		//chyba
+		return 0;
+	}
+
+	//provazeme
+	if(tape->act==tape->last){
+		//aktivni byla posledni
+		tape->last=newInstruct;
+	}else{
+		tape->act->rptr->lptr=newInstruct;
+	}
+	tape->act->rptr=newInstruct;
+	return 1;
+}
+
+int insTapePreInsert(tInsTape* tape, tInstructTypes type, void* adr1,
+		void* adr2, void* adr3){
+	if(tape->act==NULL){
+		//paska neni aktivni
+		return 0;
+	}
+
+	//pokusime se alokovat pamet novou instrukci
+	tInsTapeInsPtr newInstruct=createInstruction(type, adr1, adr2, adr3, tape->act->lptr, tape->act);
+	if(newInstruct==NULL){
+		//chyba
+		return 0;
+	}
+
+	//provazeme
+	if(tape->act==tape->first){
+		//aktivni byla prvni
+		tape->first=newInstruct;
+	}else{
+		tape->act->lptr->rptr=newInstruct;
+	}
+	tape->act->lptr=newInstruct;
+	return 1;
+}
+
+tInsTapeInsPtr insTapeGetActive(tInsTape* tape){
+	return tape->act;
+}
+
+void insTapeActualize(tInsTape* tape, tInstructTypes type, void* adr1,
+		void* adr2, void* adr3){
+
+	if(tape->act==NULL) return;
+
+	tape->act->type=tape;
+	tape->act->adr1=adr1;
+	tape->act->adr2=adr2;
+	tape->act->adr3=adr3;
+}
+
+void insTapeSucc(tInsTape* tape){
+	if(tape->act==NULL) return;
+
+	tape->act=tape->act->rptr;
+}
+
+void insTapePred(tInsTape* tape){
+	if(tape->act==NULL) return;
+
+	tape->act=tape->act->lptr;
+}
+
+int insTapeActive(tInsTape* tape){
+	return (tape->act==NULL) ? 0:1;
+}
 /*** End of file: insTape.c ***/
--- insTape.h
+++ insTape.h
@@ -50,27 +50,174 @@ typedef enum{
 
 
 /**
- * Instrukce na instrukcni pasce
+ * 3 adresova instrukce na instrukce na instrukcni pasce
  */
-typedef struct tInsTapeElem {
+typedef struct tInsTapeIns {
 	tInstructTypes type;
     void* adr1;
     void* adr2;
     void* adr3;
 
-    struct tInsTapeElem *lptr;
-    struct tInsTapeElem *rptr;
-} *tInsTapeElemPtr;
+    struct tInsTapeIns *lptr;
+    struct tInsTapeIns *rptr;
+} *tInsTapeInsPtr;
 
 /**
  * Struktura instrukcni pasky
  */
 typedef struct {
-	tInsTapeElemPtr first;
-	tInsTapeElemPtr last;
+	tInsTapeInsPtr first;	// ukazatel na prvni prvek seznamu
+	tInsTapeInsPtr act;		// ukazatel na aktualni prvek seznamu
+	tInsTapeInsPtr last;	// ukazatel na posledni prvek seznamu
 } tInsTape;
 
+/**
+ * Vytvori a inicializuje instrukcni pasku.
+ * @return Ukazatel na nove vytvorenou pasku. Pokud chyba vraci NULL.
+ */
+tInsTape* insTapeCreate();
 
+/**
+ * Uvolni instrukcni pasku z pameti.
+ * @param tape[in]	-	instrukcni paska pro uvolneni.
+ */
+void insTapeFree (tInsTape*tape);
+
+/**
+ * Vlozi instrukci na zacatek instrukcni pasky
+ * @param tape[in]	-	Paska kam vlozi instrukci.
+ * @param type[in]	-	Druh instrukce.
+ * @param adr1[in]	-	Prvni adresa.
+ * @param adr2[in]	-	Druha adresa.
+ * @param adr3[in]	-	Treti adresa.
+ * @return	Vraci 0 pokud chyba, jinak 1.
+ */
+int insTapeInsertFirst(tInsTape* tape, tInstructTypes type, void* adr1, void* adr2, void* adr3);
+
+/**
+ * Vlozi instrukci na konec instrukcni pasky
+ * @param tape[in]	-	Paska kam vlozi instrukci.
+ * @param type[in]	-	Druh instrukce.
+ * @param adr1[in]	-	Prvni adresa.
+ * @param adr2[in]	-	Druha adresa.
+ * @param adr3[in]	-	Treti adresa.
+ * @return	Vraci 0 pokud chyba, jinak 1.
+ */
+int insTapeInsertLast(tInsTape* tape, tInstructTypes type, void* adr1, void* adr2, void* adr3);
+
+/**
+ * Nastavi aktivitu na prvni instrukci v seznamu tape.
+ * @param tape[in]	-	Paska kde nastavi aktivitu.
+ */
+void insTapeFirst (tInsTape* tape);
+
+/**
+ * Nastavi aktivitu na posledni instrukci v seznamu tape.
+ * @param tape[in]	-	Paska kde nastavi aktivitu.
+ */
+void insTapeLast (tInsTape* tape);
+
+/**
+ * Ziska prvni instrukci na pasce.
+ * @param tape[in]	-	paska instrukci
+ * @return Vraci ukazatel na prvni instrukci na pasce. Vraci NULL pokud je paska prazdna.
+ */
+tInsTapeInsPtr insTapeGetFirst (tInsTape* tape);
+
+/**
+ * Ziska posledni instrukci na pasce.
+ * @param tape[in]	-	paska instrukci
+ * @return Vraci ukazatel na posledni instrukci na pasce. Vraci NULL pokud je paska prazdna.
+ */
+tInsTapeInsPtr insTapeGetLast (tInsTape* tape);
+
+/**
+ * Maze prvni instrukci na pasce. Pokud je paska prazdna nedela nic.
+ * Pokud byla prvni aktivni, aktivita se rusi.
+ * @param tape[in]	-	paska instrukci
+ */
+void insTapeDeleteFirst (tInsTape* tape);
+
+/**
+ * Maze posledni instrukci na pasce. Pokud je paska prazdna nedela nic.
+ * Pokud byla posledni aktivni, aktivita se rusi.
+ * @param tape[in]	-	paska instrukci
+ */
+void insTapeDeleteLast (tInsTape* tape);
+
+/**
+ * Maze instrukci za aktivni instrukci. Nedela nic pokud je paska prazdna nebo aktivni je posledni.
+ * Pokud je mazana instrukce aktivni rusi aktivitu.
+ * @param tape	-	paska instrukci
+ */
+void insTapePostDelete (tInsTape* tape);
+
+/**
+ * Maze instrukci pred aktivni instrukci. Nedela nic pokud je paska prazdna nebo aktivni je prvni.
+ * Pokud je mazana instrukce aktivni rusi aktivitu.
+ * @param tape	-	paska instrukci
+ */
+void insTapePreDelete (tInsTape* tape);
+
+/**
+ * Vlozi instrukci za aktivni instrukci. Pokud nebyl seznam aktivni nic se nevklada.
+ * @param tape[in]	-	Paska kam vlozi instrukci.
+ * @param type[in]	-	Druh instrukce.
+ * @param adr1[in]	-	Prvni adresa.
+ * @param adr2[in]	-	Druha adresa.
+ * @param adr3[in]	-	Treti adresa.
+ * @return	Vraci 0 pokud chyba, jinak 1.
+ */
+int insTapePostInsert (tInsTape* tape, tInstructTypes type, void* adr1, void* adr2, void* adr3);
+
+/**
+ * Vlozi instrukci pred aktivni instrukci. Pokud nebyl seznam aktivni nic se nevklada.
+ * @param tape[in]	-	Paska kam vlozi instrukci.
+ * @param type[in]	-	Druh instrukce.
+ * @param adr1[in]	-	Prvni adresa.
+ * @param adr2[in]	-	Druha adresa.
+ * @param adr3[in]	-	Treti adresa.
+ * @return	Vraci 0 pokud chyba, jinak 1.
+ */
+int insTapePreInsert (tInsTape* tape, tInstructTypes type, void* adr1, void* adr2, void* adr3);
+
+/**
+ * Ziska instrukci z pasky, ktera je nastavena jako aktivni.
+ * @param tape[in]	-	paska instrukci
+ * @return Vraci instrukci, ktera je nastavena jako aktivni. Pokud neni paska aktivni vraci NULL.
+ */
+tInsTapeInsPtr insTapeGetActive (tInsTape* tape);
+
+/**
+ * Prepise aktivni instrukci. Pokud nebyl seznam aktivni nic se neprepise.
+ * @param tape[in]	-	Paska kde aktualizuje instrukci.
+ * @param type[in]	-	Druh instrukce.
+ * @param adr1[in]	-	Prvni adresa.
+ * @param adr2[in]	-	Druha adresa.
+ * @param adr3[in]	-	Treti adresa.
+ */
+void insTapeActualize (tInsTape* tape, tInstructTypes type, void* adr1, void* adr2, void* adr3);
+
+/**
+ * Presune aktivitu na dalsi instrukci. Pri neaktivite se nestane nic.
+ * Pokud je aktivita na poslednim prvku a volam tuto funkci, paska se stava neaktivni.
+ * @param tape[in]	-	paska instrukci
+ */
+void insTapeSucc (tInsTape* tape);
+
+/**
+ * Presune aktivitu na predeslou instrukci. Pri neaktivite se nestane nic.
+ * Pokud je aktivita na prvnim prvku a volam tuto funkci, paska se stava neaktivni.
+ * @param tape[in]	-	paska instrukci
+ */
+void insTapePred (tInsTape* tape);
+
+/**
+ * Zjistuje aktivitu pasky.
+ * @param tape[in]	-	paska instrukci
+ * @return	1 pri aktivite, jinak 0;
+ */
+int insTapeActive (tInsTape* tape);
 
 #endif /* INSTAPE_H_ */
 /*** End of file: insTape.h ***/
