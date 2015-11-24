a934af4d985c0f9c5c525e583d0632c455e3dc28
Irimitenkan <RohacekD@gmail.com>
2015-11-24 13:48:39 +0100

    Interpret
    Battle continues.

---
+++ interpret.c
@@ -0,0 +1,14 @@
+#include "interpret.h"
+
+int executeTape(tInsTapeInsPtr ins) {
+	/* Prottoze tato fce je volana rekurzivne je nutno si frameStack drzet
+	 * v static
+	 */
+	static tDLList* frameStack = NULL;
+	/*Prvotni inicializace stacku*/
+	if (!frameStack) {
+		if ((tDLList *)malloc(sizeof(tDLList))) {
+			FatalError(99, ERR_MESSAGES[ERR_ALLOC]);
+		}
+	}
+}
