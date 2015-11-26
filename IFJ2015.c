cec18e21c4675f2964d51ab55468f761411f2365
tremors0 <janpavlu1@gmail.com>
2015-11-26 12:53:10 +0100

    small part of parser - need help OMG WTF

--- IFJ2015.c
+++ IFJ2015.c
@@ -1,8 +1,28 @@
 // IFJ2015.cpp : Defines the entry point for the console application.
 //
 /*
-int main()
+#include <stdlib.h>
+#include <stdio.h>
+#include "error.h"
+#include "parser.h"
+
+int main(int argc, char **argv)
 {
+    if (argc == 1 || argc >= 3) {
+        FatalError(99, "Chyba vstupnich argumentu\n");
+    }
+    
+    FILE *file;
+    
+    if ((file = fopen(argv[1], "r")) == NULL)
+    {
+        FatalError(99, "Nepodarilo se otevrit vstupni soubor\n");
+    }
+    
+    setSourceFile(file);
+    
+    parse();
+    
     return 0;
 }
 */
