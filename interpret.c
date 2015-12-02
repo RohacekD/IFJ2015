14f124666b7b1b96898476e0890ed7dd2655fec6
Irimitenkan <RohacekD@gmail.com>
2015-12-02 12:40:36 +0100

    push for dalibor

--- interpret.c
+++ interpret.c
@@ -172,19 +172,19 @@ int executeIns(tInsTapeInsPtr ins, tStack* stack) {
 		}
 		break;
 	case I_LEQUAL:
-		findVariable(stack, (string*)ins->adr1, &oper1);
-		findVariable(stack, (string*)ins->adr2, &oper2);
-		findVariable(stack, (string*)ins->adr3, &dest);
-		if (dest->type == VAR_TYPE_INT) {
-			dest->data.intVal = (int)getVarVal(oper1) <= (int)getVarVal(oper2);
-		}
-		else if (dest->type == VAR_TYPE_DOUBLE) {
-			dest->data.doubleVal = getVarVal(oper1) <= getVarVal(oper2);
-		}
-		else if (dest->type == VAR_TYPE_BOOL) {
-			dest->data.boolVal = getVarVal(oper1) <= getVarVal(oper2);
-		}
-		break;
+findVariable(stack, (string*)ins->adr1, &oper1);
+findVariable(stack, (string*)ins->adr2, &oper2);
+findVariable(stack, (string*)ins->adr3, &dest);
+if (dest->type == VAR_TYPE_INT) {
+	dest->data.intVal = (int)getVarVal(oper1) <= (int)getVarVal(oper2);
+}
+else if (dest->type == VAR_TYPE_DOUBLE) {
+	dest->data.doubleVal = getVarVal(oper1) <= getVarVal(oper2);
+}
+else if (dest->type == VAR_TYPE_BOOL) {
+	dest->data.boolVal = getVarVal(oper1) <= getVarVal(oper2);
+}
+break;
 	case I_UMINUS:
 		findVariable(stack, (string*)ins->adr1, &oper1);
 		findVariable(stack, (string*)ins->adr3, &dest);
@@ -272,9 +272,38 @@ int executeIns(tInsTapeInsPtr ins, tStack* stack) {
 void tTabSymToFrame(tBSTNodePtr node, tFrameContainer* frameContainer) {
 	if (node) {
 		tVariablePtr var;
-		tVariableType type = tTabSymToVarNotatation(((tTabSymElemData*)node->data)->info.var->dataType);
+		tVariableType type;
+		if (((tTabSymElemData*)node->data)->type == TAB_SYM_VARIABLE){
+			type = tTabSymToVarNotatation(((tTabSymElemData*)node->data)->info.var->dataType);
+			variableCreate(&var, type);
+		}
+		else if (((tTabSymElemData*)node->data)->type == TAB_SYM_CONSTANT) {
+			type = tTabSymToVarNotatation(((tTabSymElemData*)node->data)->info.constant->dataType);
+			variableCreate(&var, type);
+			switch (var->type)
+			{
+			case VAR_TYPE_INT:
+				var->data.intVal = ((tTabSymElemData*)node->data)->info.constant->value.intVal;
+				break;
+			case VAR_TYPE_BOOL:
+				var->data.boolVal = ((tTabSymElemData*)node->data)->info.constant->value.boolVal;
+
+				break;
+			case VAR_TYPE_DOUBLE:
+				var->data.doubleVal = ((tTabSymElemData*)node->data)->info.constant->value.doubleVal;
+
+				break;
+			case VAR_TYPE_STRING:
+				strInit(&var->data.stringVal);
+				strCopyString(&var->data.stringVal, ((tTabSymElemData*)node->data)->info.constant->value.stringVal);
+				break;
+			default:
+				break;
+			}
+		}
 		variableCreate(&var, type);
 		insertNewVariable(frameContainer, var,node->key);
+		var->data.intVal = 42;
 		tTabSymToFrame(node->l, frameContainer);
 		tTabSymToFrame(node->r, frameContainer);
 	}
